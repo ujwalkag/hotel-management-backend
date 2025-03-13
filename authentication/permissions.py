7c6fd23623e80aa92bf0d4fe375b802730d9b950
ujwalkag <ujwalkag99@gmail.com>
2025-03-13 21:25:16 +0530

    Delete authentication directory

--- authentication/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
